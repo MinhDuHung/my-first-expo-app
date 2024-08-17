9eb874c86091b08b132fcbe28ad73e97122a210b
minhdh1 <minh.dh1@zinza.com.vn>
2024-08-17 16:27:05 +0700

    fix: secureText and padding otp

--- components/InputComponent.tsx
+++ components/InputComponent.tsx
@@ -4,7 +4,7 @@ import Profile from "@/assets/svgs/Profile";
 import Lock from "@/assets/svgs/Lock";
 import Phone from "@/assets/svgs/Phone";
 import OpenEye from "@/assets/svgs/OpenEye";
-import { ScaledSheet } from "react-native-size-matters";
+import { scale, ScaledSheet } from "react-native-size-matters";
 
 interface Props {
   title: string;
@@ -16,12 +16,13 @@ interface Props {
 
 const InputComponent = ({ value, leftIcon, title, rightIcon, setState }: Props) => {
   const [secure, setSecure] = useState<boolean>(true);
+  const paddingLeft = leftIcon || rightIcon ? (leftIcon === "phone" ? scale(80) : scale(50)) : scale(15);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <View style={styles.inputContainer}>
         {leftIcon && LeftIcon(leftIcon)}
-        <TextInput value={value} secureTextEntry={secure} style={styles.textInput} onChangeText={(v) => setState(v)} />
+        <TextInput value={value} secureTextEntry={!secure} style={[styles.textInput, { paddingLeft }]} onChangeText={(v) => setState(v)} />
         {rightIcon && RightIcon(rightIcon, setSecure)}
       </View>
     </View>
@@ -34,7 +35,12 @@ const LeftIcon = (leftIcon: string) => {
   const icons: Record<string, React.JSX.Element> = {
     profile: <Profile />,
     lock: <Lock />,
-    phone: <Phone />,
+    phone: (
+      <View style={{ flexDirection: "row", alignItems: "center", gap: scale(5) }}>
+        <Phone />
+        <Text style={{ color: "black", fontWeight: "bold", fontSize: scale(14) }}>+84</Text>
+      </View>
+    ),
   };
   return <View style={{ position: "absolute", zIndex: 1, left: 15 }}>{icons[leftIcon]}</View>;
 };
@@ -61,7 +67,7 @@ const styles = ScaledSheet.create({
     height: "50@s",
     borderRadius: "10@s",
     backgroundColor: "white",
-    paddingHorizontal: "50@s",
+    paddingRight: "15@s",
     width: "100%",
     fontSize: "16@s",
   },
